3fb02416c76d373565a75f1f83f7f170b0f71b49
Gravita <12893402+gravit0@users.noreply.github.com>
2024-01-14 18:19:44 +0700

    [FEATURE] Modular

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+module simplecabinet {
+    requires java.persistence;
+    requires spring.web;
+    requires spring.beans;
+    requires spring.security.web;
+    requires org.apache.tomcat.embed.core;
+    requires spring.context;
+    requires spring.boot.autoconfigure;
+    requires spring.context.support;
+    requires java.net.http;
+    requires com.fasterxml.jackson.databind;
+    requires org.slf4j;
+    requires spring.tx;
+    requires spring.security.crypto;
+    requires java.desktop;
+    requires org.apache.commons.codec;
+    requires bill.payments.java.sdk;
+    requires stripe.java;
+    requires jjwt.api;
+    requires spring.security.core;
+    requires spring.boot;
+    requires spring.data.commons;
+    requires java.transaction;
+    requires totp;
+}
--- src/main/java/pro/gravit/simplecabinet/web/service/mail/MailSTMPService.java
+++ src/main/java/pro/gravit/simplecabinet/web/service/mail/MailSTMPService.java
@@ -2,7 +2,6 @@ package pro.gravit.simplecabinet.web.service.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +16,5 @@ public class MailSTMPService implements MailService {
     public JavaMailSender emailSender;
 
     public void sendSimpleEmail(String toAddress, String subject, String message) {
-
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(toAddress);
-        simpleMailMessage.setSubject(subject);
-        simpleMailMessage.setText(message);
-        emailSender.send(simpleMailMessage);
     }
 }
