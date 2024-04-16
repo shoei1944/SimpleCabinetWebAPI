9b0183843591b6cbb6ff0c0a0b5b75924c8ae7c9
Gravita <12893402+gravit0@users.noreply.github.com>
2024-04-16 16:29:50 +0700

    [FIX] Modifying

--- src/main/java/pro/gravit/simplecabinet/web/repository/ServerRepository.java
+++ src/main/java/pro/gravit/simplecabinet/web/repository/ServerRepository.java
@@ -1,6 +1,7 @@
 package pro.gravit.simplecabinet.web.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import pro.gravit.simplecabinet.web.model.Server;
 
@@ -11,9 +12,11 @@ import java.util.Optional;
 public interface ServerRepository extends JpaRepository<Server, Long> {
     Optional<Server> findByName(String name);
 
+    @Modifying
     @Query("update Server s set s.online = ?2, s.maxOnline = ?3, s.tps = ?4, s.users = ?5, s.updateDate = ?6 where s.name = ?1")
     void ping(String name, int online, int maxOnline, int tps, List<String> users, LocalDateTime updateDate);
 
+    @Modifying
     @Query("update Server s set s.online = ?2, s.maxOnline = ?3, s.tps = ?4, s.users = ?5, s.updateDate = ?6 where s.id = ?1")
     void ping(long id, int online, int maxOnline, int tps, List<String> users, LocalDateTime updateDate);
 }
